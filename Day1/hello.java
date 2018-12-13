c5b4235a1ddf801a2d238ca6d70f49b583e27df9
Ragadeep <39906721+Ragadeep@users.noreply.github.com>
2018-12-13 19:20:01 +0530

    Day 1 practiced programs

---
+++ Day1/hello.java
@@ -0,0 +1,17 @@
+class Hello{
+	
+	static void display(){
+		
+		System.out.println("disc");
+	
+	}
+	
+	public static void main(String[] args){
+		
+		display();
+		
+		System.out.println("Hello World");
+	
+	}	
+
+}
