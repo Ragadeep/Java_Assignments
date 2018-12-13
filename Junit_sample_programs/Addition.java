e4540a7ce9f3b12567e2c31598d39804411e6f9b
Ragadeep <39906721+Ragadeep@users.noreply.github.com>
2018-12-13 19:22:39 +0530

    Sample programs done by seeing Junit videos

---
+++ Junit_sample_programs/Addition.java
@@ -0,0 +1,7 @@
+package java_demo;
+
+public class Addition {
+	public int add(int number1, int number2){		//Passing integer values as parameters to this function.
+		return number1+number2;						//Returning add values to test cases
+	}
+}
